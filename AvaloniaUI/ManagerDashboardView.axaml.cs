bf4bedab9b10b421ffbc45ed335e244b4b2a0c94
Carson Brown <carsonbrown0815@gmail.com>
2026-04-17 22:28:38 -0500

    Added necessary views

---
+++ AvaloniaUI/ManagerDashboardView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaUI;
+
+public partial class ManagerDashboardView : Window
+{
+    public ManagerDashboardView()
+    {
+        InitializeComponent();
+    }
+}
