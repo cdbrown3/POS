cea929bff14e2ac425b1b3eb1c4863014dd926ba
Carson Brown <carsonbrown0815@gmail.com>
2026-04-20 13:39:30 -0500

    Interactivity between Login and Main View working

--- AvaloniaUI/LoginWindow.axaml.cs
+++ AvaloniaUI/LoginWindow.axaml.cs
@@ -1,13 +1,86 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using System;
 
 namespace AvaloniaUI;
 
-public partial class Window1 : Window
+public partial class LoginWindow : Window
 {
-    public Window1()
+    public LoginWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnButtonExitClick(object? sender, RoutedEventArgs e)
+    {
+        //For now when button exit is closed...
+        Close();
+    }
+
+    private void ManagerLoginButton_Click(object? sender, RoutedEventArgs e)
+    {
+        CreateMainFromLogin(true);
+    }
+
+    private void EmployeeLoginButton_Click(object? sender, RoutedEventArgs e)
+    {
+        CreateMainFromLogin(false);
+    }
+
+    private bool ValidatePasswordEntry()
+    {
+        Boolean retData = false;   
+        //validate password
+        string passwordEntered = passwordTextBox.Text;
+        if ((passwordEntered == null) || (passwordEntered.Length < 6))
+        {
+            retData = false;
+        }
+        else
+        {
+            retData = true;
+        }
+        return retData;
+    }
+
+    private void CreateMainFromLogin(Boolean isManager)
+    {
+        //reset color choice once the user enters a proper value
+        passwordTextBox.ClearValue(TextBox.BackgroundProperty);
+
+        if (!ValidatePasswordEntry())
+        {
+            if ((Application.Current?.TryFindResource("InvalidInputColor", out object? resource) == true) && (resource is SolidColorBrush brush))
+            {
+                passwordTextBox.Background = brush;
+            }
+        }
+        else
+        {
+            //successfully validated password
+
+            //if valid password entry then open main window
+            //consider passing in a enum for if you are manager or regular employee... maybe even just a bool.
+
+            MainWindow mainView = new MainWindow();
+
+            //copy the windows current size and position on the screen and window state like fullscreen etc...
+            mainView.Width = this.Bounds.Width;
+            mainView.Height = this.Bounds.Height;
+            mainView.Position = this.Position;
+            mainView.WindowState = this.WindowState;
+
+            if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = mainView;
+            }
+
+            mainView.Show();
+            this.Close();
+        }
     }
 }
--- AvaloniaUI/MainWindow.axaml.cs
+++ AvaloniaUI/MainWindow.axaml.cs
@@ -1,7 +1,10 @@
+using Avalonia;
 using Avalonia.Automation;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Interactivity;
 using Avalonia.Logging;
+using Avalonia.Threading;
 using POS.Backend;
 using System;
 using System.Collections.Generic;
@@ -21,31 +24,43 @@ namespace AvaloniaUI
         private Boolean _employee = true;
         private String _name = "Carson";
 
+        //System Time in top bar
+        private DispatcherTimer _clockTimer;
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
 
             Icon = new WindowIcon("resources/app_icon.ico");
-
-            //this.SizeChanged += MainWindow_SizeChanged;
+            
+            //sets up the timer for displaying time to the user.
+            IntializeTimer();
         }
 
-        //private void MainWindow_SizeChanged(object? sender, SizeChangedEventArgs e)
-        //{
-        //    String result = "";
-        //    //get current window size
-        //    double currentWidth = Bounds.Width;
-        //    double currentHeight = Bounds.Height;
 
-        //    result += "Width: " + currentWidth + " Height: " + currentHeight;
-        //    WindowSizeTextBlock.Text = result;
-        //}
-
+        //When the exit button is pressed this method creates a new login windows and closes the current main window.
         private void OnButtonExitClick(object? sender, RoutedEventArgs e)
         {
-            //For now when button exit is closed...
-            Close();
+            //Go back to the login Screen
+            //Create a new loginWindow since the old one was closed
+            LoginWindow loginView = new LoginWindow();
+
+            //preserve size, state, and position
+            loginView.Width = this.Bounds.Width;
+            loginView.Height = this.Bounds.Height;
+            loginView.Position = this.Position;
+            loginView.WindowState = this.WindowState;
+
+            //check if we are running on a desktop...
+            if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = loginView;
+            }
+
+            //show login and close this main window view
+            loginView.Show();
+            this.Close();
         }
 
         public String GetLoggedInAsMessage
@@ -66,6 +81,24 @@ namespace AvaloniaUI
                     return "Error: Could not identify user logged in... Automatically loggin out!";
                 }
             }
+        }
+
+        private void ClockTimer_Tick(object? sender, EventArgs e)
+        {
+            TimeTextBox.Text = DateTime.Now.ToString();
+        }
+
+        //Methods for initializing the timer to display to the user.
+        private void IntializeTimer()
+        {
+            //set the time intially to current time
+            TimeTextBox.Text = DateTime.Now.ToString();
+
+            //now initalize the timer to fire every second and update the display
+            _clockTimer = new DispatcherTimer();
+            _clockTimer.Interval = TimeSpan.FromSeconds(1);
+            _clockTimer.Tick += ClockTimer_Tick;
+            _clockTimer.Start();
         }
     }
 }
