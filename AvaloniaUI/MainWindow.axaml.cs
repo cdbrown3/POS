f84c81bd9b66744d36fcbb4b497f7338ba34fb08
Carson Brown <carsonbrown0815@gmail.com>
2026-04-20 10:51:41 -0500

    Added Exit button and inital Event handlers

--- AvaloniaUI/MainWindow.axaml.cs
+++ AvaloniaUI/MainWindow.axaml.cs
@@ -1,32 +1,71 @@
+using Avalonia.Automation;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Logging;
+using POS.Backend;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using POS.Backend;
 
 namespace AvaloniaUI
 {
     //all windows will inherit MainWindow
     public partial class MainWindow : Window
     {
+
+        //Temporary variables
+        private Boolean _manager = false;
+        private Boolean _employee = true;
+        private String _name = "Carson";
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
 
             Icon = new WindowIcon("resources/app_icon.ico");
+
+            //this.SizeChanged += MainWindow_SizeChanged;
         }
 
-        public String WelcomeText
+        //private void MainWindow_SizeChanged(object? sender, SizeChangedEventArgs e)
+        //{
+        //    String result = "";
+        //    //get current window size
+        //    double currentWidth = Bounds.Width;
+        //    double currentHeight = Bounds.Height;
+
+        //    result += "Width: " + currentWidth + " Height: " + currentHeight;
+        //    WindowSizeTextBlock.Text = result;
+        //}
+
+        private void OnButtonExitClick(object? sender, RoutedEventArgs e)
         {
-            get { return Model.getName() + " " + Controller.getName(); }
+            //For now when button exit is closed...
+            Close();
         }
 
         public String GetLoggedInAsMessage
         {
-            get { return "Logged in as Carson"; }
+            get 
+            {
+                if (_manager)
+                {
+                    return "Hello " + _name + ". You are logged in as a Manager with elevated privelidges.";
+                }
+                else if (_employee)
+                {
+                    return "Hello " + _name + ". You are logged in as an employee.";
+
+                }
+                else
+                {
+                    return "Error: Could not identify user logged in... Automatically loggin out!";
+                }
+            }
         }
     }
 }
--- AvaloniaUI/PaymentDialogView.axaml.cs
+++ AvaloniaUI/PaymentDialogView.axaml.cs
@@ -4,7 +4,7 @@ using Avalonia.Markup.Xaml;
 
 namespace AvaloniaUI;
 
-public partial class PaymentDialogView : MainWindow
+public partial class PaymentDialogView : Window
 {
     public PaymentDialogView()
     {
