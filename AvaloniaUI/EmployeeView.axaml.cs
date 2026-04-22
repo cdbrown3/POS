aeb2ccb3eae77620a39d51c304decd0a677622e7
Carson Brown <carsonbrown0815@gmail.com>
2026-04-22 09:34:36 -0500

    updated employee view

--- AvaloniaUI/EmployeeView.axaml.cs
+++ AvaloniaUI/EmployeeView.axaml.cs
@@ -1,6 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace AvaloniaUI;
 
@@ -10,4 +16,29 @@ public partial class EmployeeView : UserControl
     {
         InitializeComponent();
     }
+
+    public List<Customer> Customers { get; set; } = new List<Customer>()
+    {
+        new Customer("Carson", "Brown", "256-486-5343")
+    };
+}
+
+public class Customer
+{
+    //String fullname;
+    //String lname;
+    //String phone;
+
+    public Customer(String fname, String lname, String phone)
+    {
+        Fullname = fname;
+        Lastname = lname;
+        Phone = phone;
+    }
+
+    public String Fullname { get; set; }
+
+    public String Lastname { get; set; }
+
+    public String Phone { get; set; }
 }
